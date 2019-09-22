a9ba7a5161d0deecf604afc28f6fb8250db7a094
unknown <gfram@hotmail.com>
2019-09-22 14:36:24 +0100

    first commit

---
+++ PlayingWithArrays/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace PlayingWithArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] names = { "Tom", "Frank", "West", };
+
+            Console.WriteLine("{1}", names);
+
+        }
+    }
+}
